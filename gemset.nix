33e8d19fb229deec7dd98b3d2cd309a5fb716dd0
Rohit Goswami <rgoswami@ieee.org>
2020-08-19 03:11:09 +0000

    nix: Init a pure shell builder

---
+++ gemset.nix
@@ -0,0 +1,12 @@
+{
+  rake = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0w6qza25bq1s825faaglkx1k6d59aiyjjk3yw3ip5sb463mhhai9";
+      type = "gem";
+    };
+    version = "13.0.1";
+  };
+}
