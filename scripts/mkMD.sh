b935e5b00c295a36bcc98ba79a650af1e9310150
Rohit Goswami <rgoswami@ieee.org>
2020-08-19 02:02:33 +0000

    pandoc: Bring back pandoc scripts

--- scripts/mkMD.sh
+++ scripts/mkMD.sh
@@ -4,6 +4,9 @@
 #        2 is the orgmode setup file,
 #        3 is the temporary directory
 
+pandoc --version
+pandoc-citeproc --version
+
 emacs --batch --eval \
 	"(progn (setenv \"OX_HUGO_TMP_DIR\" \"$3\") \
     (load-file (expand-file-name \"setup-ox-hugo.el\" \"$2\" )))" \
