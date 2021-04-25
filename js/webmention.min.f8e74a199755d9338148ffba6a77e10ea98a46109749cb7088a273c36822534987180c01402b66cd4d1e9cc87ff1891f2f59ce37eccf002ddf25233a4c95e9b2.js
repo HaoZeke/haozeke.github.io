b93e46b648e6f551f1fb0eabf6e734e728d7a4ab
HaoZeke <HaoZeke@users.noreply.github.com>
2021-04-25 04:01:26 +0000

    deploy: 956b2163215489a59a130011364f58facf1cb9f9

---
+++ js/webmention.min.f8e74a199755d9338148ffba6a77e10ea98a46109749cb7088a273c36822534987180c01402b66cd4d1e9cc87ff1891f2f59ce37eccf002ddf25233a4c95e9b2.js
@@ -0,0 +1,40 @@
+(function(){"use strict";function getCfg(key,dfl){return document.currentScript.getAttribute("data-"+key)||dfl;}
+var refurl=getCfg("page-url",window.location.href.replace(/#.*$/,""));var addurls=getCfg("add-urls",undefined);var containerID=getCfg("data-id","webmentions");var textMaxWords=getCfg("wordcount");var maxWebmentions=getCfg("max-webmentions",30);var mentionSource=getCfg("prevent-spoofing")?"wm-source":"url";var sortBy=getCfg("sort-by","published");var sortDir=getCfg("sort-dir","up");var reactTitle={"in-reply-to":"replied","like-of":"liked","repost-of":"reposted","bookmark-of":"bookmarked","mention-of":"mentioned",rsvp:"RSVPed","follow-of":"followed",};var reactEmoji={"in-reply-to":"💬","like-of":"❤️","repost-of":"🔄","bookmark-of":"⭐️","mention-of":"💬",rsvp:"📅","follow-of":"🐜",};var rsvpEmoji={yes:"✅",no:"❌",interested:"💡",maybe:"💭",};function entities(text){return text.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"&quot;");}
+function reactImage(r){var who=entities(r.author&&r.author.name?r.author.name:r.url.split("/")[2]);var response=reactTitle[r["wm-property"]]||"reacted";var html='<a class="reaction" rel="nofollow ugc" title="'+
+who+
+" "+
+response+
+'" href="'+
+r[mentionSource]+
+'">';if(r.author&&r.author.photo){html+='<img src="'+entities(r.author.photo)+'">';}
+html+=reactEmoji[r["wm-property"]]||"💥";if(r.rsvp&&rsvpEmoji[r.rsvp]){html+="<sub>"+rsvpEmoji[r.rsvp]+"</sub>";}
+html+="</a>";return html;}
+function stripurl(url){return url.substr(url.indexOf("//"));}
+function dedupe(mentions){var filtered=[];var seen={};mentions.forEach(function(r){var source=stripurl(r.url);if(!seen[source]){filtered.push(r);seen[source]=true;}});return filtered;}
+function formatComments(comments){var html="<h2>"+
+comments.length+
+" Response"+
+(comments.length>1?"s":"")+
+'</h2><ul class="comments">';comments.forEach(function(c){html+="<li>";html+=reactImage(c);html+=' <a class="source" rel="nofollow ugc" href="'+c[mentionSource]+'">';if(c.author&&c.author.name){html+=entities(c.author.name);}else{html+=entities(c.url.split("/")[2]);}
+html+="</a>: ";var linkclass;var linktext;if(c.name){linkclass="name";linktext=c.name;}else if(c.content&&c.content.text){var text=entities(c.content.text);if(textMaxWords){var words=text.replace(/\s+/g," ").split(" ",textMaxWords+1);if(words.length>textMaxWords){words[textMaxWords-1]+="&hellip;";words=words.slice(0,textMaxWords);text=words.join(" ");}}
+linkclass="text";linktext=text;}else{linkclass="name";linktext="(mention)";}
+html+='<span class="'+linkclass+'">'+linktext+"</span>";html+="</li>";});html+="</ul>";return html;}
+function formatReactions(reacts){var html="<h2>"+
+reacts.length+
+" Reaction"+
+(reacts.length>1?"s":"")+
+'</h2><ul class="reacts">';reacts.forEach(function(r){html+=reactImage(r);});return html;}
+function getData(url,callback){if(window.fetch){window.fetch(url).then(function(response){if(response.status>=200&&response.status<300){return Promise.resolve(response);}else{return Promise.reject(new Error(response.statusText));}}).then(function(response){return response.json();}).then(callback).catch(function(error){console.error("Request failed",error);});}else{var oReq=new XMLHttpRequest();oReq.onload=function(data){callback(JSON.parse(data));};oReq.onerror=function(error){console.error("Request failed",error);};}}
+window.addEventListener("load",function(){var container=document.getElementById(containerID);if(!container){return;}
+var pages=[stripurl(refurl)];if(!!addurls){addurls.split("|").forEach(function(url){pages.push(stripurl(url));});}
+var apiURL="https://webmention.io/api/mentions.jf2?per-page="+
+maxWebmentions+
+"&sort-by="+
+sortBy+
+"&sort-dir="+
+sortDir;pages.forEach(function(path){apiURL+="&target[]="+
+encodeURIComponent("http:"+path)+
+"&target[]="+
+encodeURIComponent("https:"+path);});getData(apiURL,function(json){var html="";var comments=[];var collects=[];var mapping={"in-reply-to":comments,"like-of":collects,"repost-of":collects,"bookmark-of":collects,"mention-of":comments,rsvp:comments,};json.children.forEach(function(c){var store=mapping[c["wm-property"]];if(store){store.push(c);}});if(comments.length>0){html+=formatComments(dedupe(comments));}
+if(collects.length>0){html+=formatReactions(dedupe(collects));}
+container.innerHTML=html;});});})();
