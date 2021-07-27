c97dea2de452b80d126d7f6fbeba6b2cdb9aa16c
Rohit Goswami <rog32@hi.is>
2021-07-27 15:55:34 +0000

    nix: Pin pandoc by pinning haskellpackages

--- default.nix
+++ default.nix
@@ -1,6 +1,13 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
+  # haskellpkgs is a pinned version of nixpkgs
+  # https://github.com/HaoZeke/haozeke.github.io/commit/e312ff597931fde168e4da6aec37164ef2616894#diff-a0745f7ed88cfac6058d1d4cfb57bc71127a28ad7c6756004142a90aab5ee99f
+  hpkgs = import sources.haskellpkgs {};
+  myHaskellEnv = hpkgs.haskellPackages.ghcWithPackages
+    (haskellPackages: with haskellPackages; [ pandoc_2_10_1 pandoc-citeproc ]);
+  # Don't go beyond 2.10.1 until https://github.com/kaushalmodi/ox-hugo/issues/336 is closed
+  # https://github.com/vaibhavsagar/website/blob/master/blog/quick-easy-nixpkgs-pinning.md
   stdenv = pkgs.stdenv;
   myGems = pkgs.bundlerEnv {
     name = "gems-for-some-project";
@@ -34,11 +41,12 @@ in pkgs.stdenv.mkDerivation {
     # Ruby
     myGems
     (lowPrio myGems.wrappedRuby)
-    pandoc
+    myHaskellEnv
   ];
 
   configurePhase = ''
     mkdir -p "$(pwd)/_libs"
+    eval $(egrep ^export ${myHaskellEnv}/bin/ghc)
     export R_LIBS_USER="$(pwd)/_libs"
     ln -s ${nodePkgs}/lib/node_modules ./node_modules
     export PATH="${nodePkgs}/bin:$PATH"
--- shell.nix
+++ shell.nix
@@ -1,6 +1,14 @@
 let
+  # niv
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
+  # haskellpkgs is a pinned version of nixpkgs
+  # https://github.com/HaoZeke/haozeke.github.io/commit/e312ff597931fde168e4da6aec37164ef2616894#diff-a0745f7ed88cfac6058d1d4cfb57bc71127a28ad7c6756004142a90aab5ee99f
+  hpkgs = import sources.haskellpkgs {};
+  myHaskellEnv = hpkgs.haskellPackages.ghcWithPackages
+    (haskellPackages: with haskellPackages; [ pandoc_2_10_1 pandoc-citeproc ]);
+  # Don't go beyond 2.10.1 until https://github.com/kaushalmodi/ox-hugo/issues/336 is closed
+  # https://github.com/vaibhavsagar/website/blob/master/blog/quick-easy-nixpkgs-pinning.md
   nodePkgs = (pkgs.callPackage ./node.nix {
     inherit pkgs;
     nodejs = pkgs.nodejs-12_x;
@@ -13,6 +21,7 @@ let
   hook = ''
     mkdir -p "$(pwd)/_libs"
     export R_LIBS_USER="$(pwd)/_libs"
+    eval $(egrep ^export ${myHaskellEnv}/bin/ghc)
     ln -s ${nodePkgs}/lib/node_modules ./node_modules
     export PATH="${nodePkgs}/bin:$PATH"
   '';
@@ -32,7 +41,7 @@ in pkgs.mkShell {
     # Ruby
     myGems
     (lowPrio myGems.wrappedRuby)
-    pandoc
+    myHaskellEnv
   ];
   shellHook = hook;
   GIT_SSL_CAINFO = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
