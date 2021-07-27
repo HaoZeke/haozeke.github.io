79d4b0ff8511d59ddb41be732cd308709c5f6b2c
Rohit Goswami <rog32@hi.is>
2021-07-27 16:25:27 +0000

    nix: Update rev correctly

--- shell.nix
+++ shell.nix
@@ -5,8 +5,6 @@ let
   # haskellpkgs is a pinned version of nixpkgs
   # https://github.com/HaoZeke/haozeke.github.io/commit/e312ff597931fde168e4da6aec37164ef2616894#diff-a0745f7ed88cfac6058d1d4cfb57bc71127a28ad7c6756004142a90aab5ee99f
   hpkgs = import sources.haskellpkgs {};
-  myHaskellEnv = hpkgs.haskellPackages.ghcWithPackages
-    (haskellPackages: with haskellPackages; [ pandoc_2_10_1 pandoc-citeproc ]);
   # Don't go beyond 2.10.1 until https://github.com/kaushalmodi/ox-hugo/issues/336 is closed
   # https://github.com/vaibhavsagar/website/blob/master/blog/quick-easy-nixpkgs-pinning.md
   nodePkgs = (pkgs.callPackage ./node.nix {
@@ -21,7 +19,6 @@ let
   hook = ''
     mkdir -p "$(pwd)/_libs"
     export R_LIBS_USER="$(pwd)/_libs"
-    eval $(egrep ^export ${myHaskellEnv}/bin/ghc)
     ln -s ${nodePkgs}/lib/node_modules ./node_modules
     export PATH="${nodePkgs}/bin:$PATH"
   '';
@@ -41,8 +38,7 @@ in pkgs.mkShell {
     # Ruby
     myGems
     (lowPrio myGems.wrappedRuby)
-    myHaskellEnv
-  ];
+  ] ++ [ hpkgs.pandoc ];
   shellHook = hook;
   GIT_SSL_CAINFO = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
   LOCALE_ARCHIVE = pkgs.lib.optionalString stdenv.isLinux
