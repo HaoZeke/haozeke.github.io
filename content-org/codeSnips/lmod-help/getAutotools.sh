7e6399aa9d5b434e601923e84f41c4111402e17e
Rohit Goswami <rog32@hi.is>
2021-08-06 19:51:19 +0000

    codesnips: Update

--- content-org/codeSnips/lmod-help/getAutotools.sh
+++ content-org/codeSnips/lmod-help/getAutotools.sh
@@ -1,21 +1,24 @@
-mkdir -p ~/tmpHPC
-cd $HOME/tmpHPC
+cd $hpcroot
 myprefix=$HOME/.hpc/autotools
 export PATH
-wget http://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.gz
 wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 wget http://ftp.gnu.org/gnu/automake/automake-1.16.2.tar.gz
 wget http://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.gz
-gzip -dc m4-1.4.18.tar.gz | tar xvf -
+wget http://ftp.gnu.org/gnu/gettext/gettext-0.20.tar.gz
+wget https://gnuftp.uib.no/autoconf-archive/autoconf-archive-2021.02.19.tar.xz
+tar xfv autoconf-archive-2021.02.19.tar.xz
 gzip -dc autoconf-2.69.tar.gz | tar xvf -
 gzip -dc automake-1.16.2.tar.gz | tar xvf -
 gzip -dc libtool-2.4.6.tar.gz | tar xvf -
-cd m4-1.4.18
-./configure -C --prefix=$myprefix/m4/1.4.18 && make -j$(nproc) && make install
-cd ../autoconf-2.69
+gzip -dc gettext-0.20.tar.gz | tar xvf -
+cd autoconf-2.69
 ./configure -C --prefix=$myprefix/autoconf/2.69 && make -j$(nproc) && make install
 cd ../automake-1.16.2
-./configure -C --prefix=$myprefix/automake/1.16.2 && make -j$(nproc) && make install
+./configure -C --prefix=$myprefix/automake/1.16.2 --docdir=$myprefix/automake/1.16.2/share/doc/automake-1.16.2 && make -j$(nproc) && make install
+cd ../autoconf-archive-2021.02.19
+./configure -C --prefix=$myprefix/automake/1.16.2
 cd ../libtool-2.4.6
-./configure -C --prefix=$myprefix/libtool/2.4.6 && make -j$(nproc) && make install
+./configure -C --disable-static --prefix=$myprefix/libtool/2.4.6 && make -j$(nproc) && make install
+cd ../gettext-0.20
+./configure -C --prefix=$myprefix/gettext/0.20 && make -j$(nproc) && make install
 ml load autotools/autotools
--- content-org/codeSnips/lmod-help/getPerl.sh
+++ content-org/codeSnips/lmod-help/getPerl.sh
@@ -1,6 +1,6 @@
 # Get Perl
 curl -L http://xrl.us/installperlnix | bash
-# Use Perl
-ml use perl/5.28.0
-cpanm ExtUtils::MakeMaker # For git
+cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+cpanm ExtUtils::MakeMaker --force # For git
+cpanm Thread::Queue # For automake
 ml load perl/5.28.0
