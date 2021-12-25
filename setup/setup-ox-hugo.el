f0090cdb692ff0ce932fcf831f9b85c8d1decbd4
Rohit Goswami <rog32@hi.is>
2021-12-25 07:43:55 +0000

    BLD: More settings

--- setup/setup-ox-hugo.el
+++ setup/setup-ox-hugo.el
@@ -5,7 +5,9 @@
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-                         ("melpa" . "https://melpa.org/packages/")))
+                         ("elpa" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ))
 (package-initialize)
 
 ;; load use-package and check if it's installed, if not downloads it
@@ -17,11 +19,13 @@
 ;; Install newer version of org-mode
 (use-package org
   :pin elpa
+  :ensure t
   :commands (org-mode org-capture org-agenda orgbl-mode)
   :mode (("\\.org$" . org-mode)))
 
 (use-package org-contrib
   :pin nongnu
+  :ensure t
   :after org)
 
 ;; Some sane settings
