ec221a3cfa5285fe41ce2a9fc2d2062585b91610
HaoZeke <HaoZeke@users.noreply.github.com>
2022-04-23 23:03:00 +0000

    deploy: e7ac7141047df45b381953a2b8aba3d2c40e2a91

---
+++ js/codefold.min.d0515ab08b3615633a8e8628e085c3c753ffc806b6d58bc2221083be6e9ab17a7a86ac9b4fc772c93a44f5d665dc13e87de3bc389b27c736e2a6a46e0df12f9c.js
@@ -0,0 +1,1 @@
+new Readmore(".highlight",{speed:75,heightMargin:25,lessLink:'<div class="fold"><div class="button"><div class="button__text fold"><a href="#">↑↑</a></div></div></div>',moreLink:'<div class="fold"><div class="button"><div class="button__text fold"><a href="#">↓↓</a></div></div></div>',afterToggle:function(n,e,t){t||window.scrollTo({top:e.offsetTop,behavior:"smooth"})}})
