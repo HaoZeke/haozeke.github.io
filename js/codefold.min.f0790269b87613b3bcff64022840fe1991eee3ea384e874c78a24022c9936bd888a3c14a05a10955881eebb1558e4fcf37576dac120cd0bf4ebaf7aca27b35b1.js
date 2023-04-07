713e23bcd76f3a8decb55bf68b3ce80639fb025e
Rohit Goswami <rog32@hi.is>
2023-04-07 20:39:31 +0000

    MAINT: Update files

---
+++ js/codefold.min.f0790269b87613b3bcff64022840fe1991eee3ea384e874c78a24022c9936bd888a3c14a05a10955881eebb1558e4fcf37576dac120cd0bf4ebaf7aca27b35b1.js
@@ -0,0 +1,1 @@
+new Readmore(".highlight",{speed:75,heightMargin:25,lessLink:'<div class="fold"><div class="button"><div class="button__text fold"><a href="#">↑↑</a></div></div></div>',moreLink:'<div class="fold"><div class="button"><div class="button__text fold"><a href="#">↓↓</a></div></div></div>',afterToggle:function(e,t,n){n||window.scrollTo({top:t.offsetTop,behavior:"smooth"})}})
